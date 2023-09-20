706dd23b26a54174975341a0ad3aa2a8e5f47f8f
VishPunwani <111029896+VishPunwani@users.noreply.github.com>
2023-09-20 00:29:03 -0700

    followed along with tutorial to fix bugs

--- index.js
+++ index.js
@@ -1,101 +1,120 @@
 const apiKey = '6aec77677605d2cda5029a345623edd6';
-const imgKey = 'https://image.tmdb.org/t/p/w1280';
-const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
-const form = document.getElementById('search-form');
-const query = document.getElementById('search-input');
-const result = document.getElementById('result');
+const imgApi = "https://image.tmdb.org/t/p/w1280";
+const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=`;
+const form = document.getElementById("search-form");
+const query = document.getElementById("search-input");
+const result = document.getElementById("result");
 
 let page = 1;
 let isSearching = false;
 
+// Fetch JSON data from url
 async function fetchData(url) {
-    try{
+    try {
         const response = await fetch(url);
-        if(!response.ok){
-            throw new Error('Network response failed');
+        if (!response.ok) {
+            throw new Error("Network response was not ok.");
         }
         return await response.json();
-    } catch(error){
+    } catch (error) {
         return null;
     }
 }
 
-
-async function fetchAndShowResults(url) {
+// Fetch and show results based on url
+async function fetchAndShowResult(url) {
     const data = await fetchData(url);
     if (data && data.results) {
         showResults(data.results);
     }
-    console.log(showResults)
 }
 
-function createMovieCard(movie){
-    const{posterPath, originalTitle, releaseDate, overview} = movie;
-    const imagePath = posterPath ? imgAPI + posterPath : "./img-01.jpeg";
-    const truncatedTitle = originalTitle.length > 15 ? originalTitle.slice(0 , 15)
- + "..." : originalTitle;
- const formattedDate = releaseDate || "No release date";
- const cardTemplate = `
+// Create movie card html template
+function createMovieCard(movie) {
+    const { poster_path, original_title, release_date, overview } = movie;
+    const imagePath = poster_path ? imgApi + poster_path : "./img-01.jpeg";
+    const truncatedTitle = original_title.length > 15 ? original_title.slice(0, 15) + "..." : original_title;
+    const formattedDate = release_date || "No release date";
+    const cardTemplate = `
         <div class="column">
             <div class="card">
                 <a class="card-media" href="./img-01.jpeg">
                     <img src="${imagePath}" alt="${original_title}" width="100%" />
                 </a>
                 <div class="card-content">
-                <div class="card-header">
-                    <div class="left-content">
-                    <h3 style="font-weight: 600">${truncatedTitle}</h3>
-                    <span style="color: #12efec">${formattedDate}</span>
+                    <div class="card-header">
+                        <div class="left-content">
+                        <h3 style="font-weight: 600">${truncatedTitle}</h3>
+                        <span style="color: #12efec">${formattedDate}</span>
+                        </div>
+                    <div class="right-content">
+                        <a href="${imagePath}" target="_blank" class="card-btn">See Cover</a>
                     </div>
-                <div class="right-content">
-                    <a href="${imagePath}" target="_blank" class="card-btn">See Cover</a>
                 </div>
-            </div>
-            <div class="info">
-                ${overview || "No overview yet..."}
+                <div class="info">
+                    ${overview || "No overview yet..."}
+                </div>
             </div>
         </div>
     </div>
-</div>
-`;
-return cardTemplate;
+    `;
+    return cardTemplate;
 }
 
-
+// Clear result element for search
 function clearResults() {
     result.innerHTML = "";
 }
 
+// Show results in page
 function showResults(item) {
-const newContent = item.map(createMovieCard).join("");
-result.innerHTML = newContent || "<p>No Results Found</p>";
+    const newContent = item.map(createMovieCard).join("");
+    result.innerHTML += newContent || "<p>No results found.</p>";
 }
 
+// Load more results
 async function loadMoreResults() {
-    if(isSearching){
+    if (isSearching) {
         return;
     }
     page++;
     const searchTerm = query.value;
-    const url = searchTerm ? `${searchURL}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
-    await fetchAndShowSearchResults(url);
+    const url = searchTerm ? `${searchUrl}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    await fetchAndShowResult(url);
 }
 
-function detectEnd(){
-    const {scrollTop, clientHeight, scrollHeight} = document.documentElement;
-    if(scrollHeight + clientHeight >= scrollHeight - 20){
+// Detect end of page and load more results
+function detectEnd() {
+    const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 20) {
         loadMoreResults();
     }
 }
 
-async function handleSearch(e){
+// Handle search
+async function handleSearch(e) {
     e.preventDefault();
     const searchTerm = query.value.trim();
-    if(searchTerm){
+    if (searchTerm) {
         isSearching = true;
         clearResults();
-        const newURL = `${searchURL}${searchTerm}&page=${page}`;
-        await fetchAndShowSearchResults(newURL);
-        query.value=""
+        const newUrl = `${searchUrl}${searchTerm}&page=${page}`;
+        await fetchAndShowResult(newUrl);
+        query.value = "";
     }
 }
+
+// Event listeners
+form.addEventListener('submit', handleSearch);
+window.addEventListener('scroll', detectEnd);
+window.addEventListener('resize', detectEnd);
+
+// Initialize the page
+async function init() {
+    clearResults();
+    const url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
+    isSearching = false;
+    await fetchAndShowResult(url);
+}
+
+init();
