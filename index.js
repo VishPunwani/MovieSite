8eefed6a6b1a7a3c4f8878a8d1196af563b0f4ad
VishPunwani <111029896+VishPunwani@users.noreply.github.com>
2023-09-20 00:12:58 -0700

    adding pagnation

--- index.js
+++ index.js
@@ -41,6 +41,42 @@ function createMovieCard(movie){
                 <a class="card-media" href="./img-01.jpeg">
                     <img src="${imagePath}" alt="${original_title}" width="100%" />
                 </a>
-        `
+                <div class="card-content">
+                <div class="card-header">
+                    <div class="left-content">
+                    <h3 style="font-weight: 600">${truncatedTitle}</h3>
+                    <span style="color: #12efec">${formattedDate}</span>
+                    </div>
+                <div class="right-content">
+                    <a href="${imagePath}" target="_blank" class="card-btn">See Cover</a>
+                </div>
+            </div>
+            <div class="info">
+                ${overview || "No overview yet..."}
+            </div>
+        </div>
+    </div>
+</div>
+`;
+return cardTemplate;
+}
+
+
+function clearResults() {
+    result.innerHTML = "";
+}
+
+function showResults(item) {
+const newContent = item.map(createMovieCard).join("");
+result.innerHTML = newContent || "<p>No Results Found</p>";
+}
+
+async function loadMoreResults() {
+    if(isSearching){
+        return;
+    }
+    page++;
+    const searchTerm = query.value;
+    const url = searchTerm ? `${searchURL}${searchTerm}&page=${page}` : `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${apiKey}&page=${page}`;
 
 }
